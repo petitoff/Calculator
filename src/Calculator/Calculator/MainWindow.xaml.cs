a41304d4edc552985a48bea564f86b2ec141992a
Błażej Domagała <petitoff@outlook.com>
2022-09-05 11:42:04 +0200

    updated

--- src/Calculator/Calculator/MainWindow.xaml.cs
+++ src/Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            acButton.Click += ClearResult_Button;
+            negativeButton.Click += NegativeConvert_Button;
+            percentageButton.Click += PercentageCalculate_Button;
+            equalButton.Click += equal_Button;
+        }
+
+        private void equal_Button(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PercentageCalculate_Button(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber /= 100;
+                ResultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeConvert_Button(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber *= -1;
+                ResultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void ClearResult_Button(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = "0";
+        }
+
+        private void NumberEvent_Button(object sender, RoutedEventArgs e)
+        {
+            var content = (sender as Button).Content;
+            if(ResultLabel.Content.ToString() == "0")
+            {
+                ResultLabel.Content = content as string;
+            }
+            else
+            {
+                ResultLabel.Content += content as string;
+
+            }
         }
     }
 }
